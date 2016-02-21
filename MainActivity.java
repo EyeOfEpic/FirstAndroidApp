4c081d967fe7e561ce6227a9967169595bc8f394
EyeOfEpic <felixsebastianraab@gmail.com>
2016-02-21 14:31:42 +0100

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.felix.simpletoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void sendMessage(View view) {
+        Toast.makeText(MainActivity.this, "Hallo :D", Toast.LENGTH_LONG).show();
+    }
+
+}
